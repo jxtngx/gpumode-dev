30927740dc53ee51f661cce7e5bfe9f4efff912f
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2025-01-02 23:50:24 -0500

    update irl

--- site/app/irl/page.tsx
+++ site/app/irl/page.tsx
@@ -1,42 +1,47 @@
 import Link from "next/link";
-
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
+import ArrowIcon from 'app/components/arrowicon'
 
 export default function Page() {
-  return (
-      <section>
-          <h1 className="font-semibold text-2xl mb-8 tracking-tighter">IRL Events</h1>
-          <div>
-              <Link className="flex flex-col space-y-2 mb-4" href="" target="_blank">
+    return (
+        <section>
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">IRL Events</h1>
+            <h2 className="font-semibold text-lg mb-8 tracking-tighter">Upcoming IRLs</h2>
+            <div>
+            <Link className="flex flex-col space-y-2 mb-4" href="" target="_blank">
                   <div className="w-full flex flex-col items-center md:flex-row space-x-0 md:space-x-2">
                       <ArrowIcon/>
                       <hr></hr>
-                      <p className="text-neutral-900 dark:text-neutral-100 w-[300px]">
-                          A very, very, very long event title
+                      <p className="text-neutral-900 dark:text-neutral-100 w-[100px]">
+                          Stay tuned!
                       </p>
                       <p className="text-neutral-600 dark:text-neutral-400 w-[125px] tabular-nums">
-                          City
+                          TBD location
                       </p>
-                      <p className="text-neutral-600 dark:text-neutral-400 w-[125px] tabular-nums">
-                          Date, Time
+                      <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
+                          TBD date and time
                       </p>
                   </div>
               </Link>
+          </div>
+          <br/>
+          <p className="mb-4">
+              {` Subscribe to the `}
+              <Link href="https://lu.ma/gpumode" target="_blank">
+                  <u>Luma calendar</u>
+              </Link>
+              {` for upcoming events. `}
+          </p>
+          <br/>
+          <h2 className="font-semibold text-lg mb-8 tracking-tighter">Past IRLs</h2>
+          <div>
+              <iframe
+                  width="560" height="315"
+                  src="https://www.youtube.com/embed/FH5wiwOyPX4?si=FiiHl2JN18Au7xun"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen>
+              </iframe>
           </div>
       </section>
   )
