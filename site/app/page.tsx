5d3d71bf8a6208ccb966d413cc67ab2cf2517f8f
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2025-01-02 23:50:34 -0500

    update home page

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -2,16 +2,26 @@ import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        GPU Mode
-      </h1>
-      <p className="mb-4">
-        {`Welcome to GPU Mode, your favorite GPU community!`}
-      </p>
-      <div className="my-8">
-        <BlogPosts/>
-      </div>
-    </section>
+      <section>
+          <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+              GPU Mode
+          </h1>
+          <p className="mb-4">
+              {`Welcome to GPU Mode, your favorite GPU community!`}
+          </p>
+          <div className="my-8">
+              <BlogPosts/>
+          </div>
+          <div>
+              <iframe
+                  width="560" height="315"
+                  src="https://www.youtube.com/embed/FH5wiwOyPX4?si=FiiHl2JN18Au7xun"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen>
+              </iframe>
+          </div>
+      </section>
   )
 }
