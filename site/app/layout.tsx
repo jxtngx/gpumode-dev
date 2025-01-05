0338e36ec9c47da8e2b253bfa815dc1a53666250
Justin Goheen <26209687+jxtngx@users.noreply.github.com>
2025-01-05 17:54:10 -0500

    remove @next/font for in-built /font

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { GeistMono } from "geist/font/mono"
+import { GeistSans } from "geist/font/sans"
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -9,9 +9,9 @@ import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import { ThemeProvider } from './components/theme-provider'
 
-// Use Geist fonts
 const fontSans = GeistSans
 const fontMono = GeistMono
+
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
